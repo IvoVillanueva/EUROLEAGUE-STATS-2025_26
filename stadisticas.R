544aa19eb14f22225d11a554946d6cd7b102f32d
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-10-26 12:25:26 +0100

    Update stadisticas.R

--- stadisticas.R
+++ stadisticas.R
@@ -1,6 +1,11 @@
+#librerias
 library(httr)
 library(tidyverse)
 
+# asegurar que la carpeta data existe
+if (!dir.exists("data")) dir.create("data")
+
+#código
 euroligaStats <- GET("JSON_STATS_EUROLEAGE") %>%
   content() %>% 
   pluck("players") %>%
@@ -8,3 +13,6 @@ euroligaStats <- GET("JSON_STATS_EUROLEAGE") %>%
   unnest_wider(value) %>% 
   unnest_wider(player) %>% 
   unnest_wider(team, names_sep = "_")
+
+# escribir el dataframe en la carpeta data
+write.csv(euroligaStats, paste0("data/euroligaStats.csv"), row.names = F)
