544f062483835f24a175430a226e28f30953a01b
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-11-25 13:08:08 +0100

    Delete main.R

--- main.R
+++
@@ -1,6 +0,0 @@
-source(c("stadisticas_eurocup.R", "stadisticas_euroleague.R"))
-
-
-# escribir csvs
-write.csv(euroliga_stats, "data/euroliga_stats.csv", row.names = F)
-write.csv(eurocup_stats, "data/eurocup_stats.csv", row.names = F)
